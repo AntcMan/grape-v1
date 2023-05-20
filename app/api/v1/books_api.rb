2d4dc02961365bc493dd1cebe6c447b873e151a4
Anthony Marion <anthonycmarion@gmail.com>
2023-05-20 15:43:48 +1000

    books api file seeded with sample data for testing

--- app/api/v1/books_api.rb
+++ app/api/v1/books_api.rb
@@ -5,6 +5,15 @@ class BooksAPI < Grape::API
   version 'V1', using: path
   format :json
 
+   # DEFINE A COLLECTION OF BOOKS
+   BOOKS = [
+    { id: 1, title: 'Harry Potter and the Red Stone'}
+    { id: 2, title: 'Harry Potter and the Chamber of Secrets'}
+    { id: 3, title: 'Harry Potter and the Prisoner of Azkaban'}
+    { id: 4, title: 'Harry Potter and the Goblet of Fire'}
+    { id: 5, title: 'Harry Potter and the Order of the Phoenix'}
+   ]
+
   resource :books do
     desc 'Get all books'
     get do
